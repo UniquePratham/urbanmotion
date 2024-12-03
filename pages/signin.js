da1dda3ff66c10e7d228c0e6a52887801af8a904
subhro1530 <shaswata.ssaha@gmail.com>
2024-12-04 00:04:05 +0530

    made signup perfectly

--- pages/signin.js
+++ pages/signin.js
@@ -5,176 +5,108 @@ import {
   Heading,
   Input,
   Text,
-  Icon,
   Link,
+  useToast,
 } from "@chakra-ui/react";
-import {
-  FaFacebook,
-  FaEye,
-  FaEyeSlash,
-  FaGoogle,
-  FaLinkedin,
-} from "react-icons/fa";
 import { motion } from "framer-motion";
 import { useState } from "react";
 
 const MotionBox = motion(Box);
 
 const SignIn = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const toast = useToast();
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSignIn = async () => {
+    try {
+      const response = await fetch(
+        "https://urban-motion-backend.vercel.app/api/verify-customer",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
+      const result = await response.json();
+
+      if (response.ok) {
+        toast({
+          title: "Success",
+          description: result.message || "Logged in successfully.",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        throw new Error(result.message || "Invalid login credentials.");
+      }
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
   };
 
   return (
     <Flex
       minH="100vh"
       bg="gray.900"
-      direction={{ base: "column", md: "row" }} // Stack on small screens
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
     >
-      {/* Left Side */}
       <MotionBox
-        flex="1"
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        px={{ base: 4, md: 8 }}
-        py={{ base: 8, md: 0 }} // Add padding for small screens
-        initial={{ opacity: 0, x: -50 }}
-        animate={{ opacity: 1, x: 0 }}
+        px={6}
+        py={8}
+        borderRadius="lg"
+        bg="gray.800"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 0.8 }}
-        bg={{ base: "gray.800", md: "transparent" }} // Dim background on mobile
-        borderRadius={{ base: "md", md: "none" }} // Add rounded corners for mobile
       >
-        <Heading color="white" mb={4} fontSize={{ base: "2xl", md: "4xl" }}>
-          Login to Your Account
+        <Heading color="white" mb={6} textAlign="center">
+          Sign In
         </Heading>
-        <Text color="gray.400" mb={6} fontSize={{ base: "sm", md: "md" }}>
-          Login using social networks
-        </Text>
-        <Flex
-          justify="center"
-          mb={6}
-          gap={4}
-          direction={{ base: "column", md: "row" }}
-        >
-          <Button
-            leftIcon={<FaFacebook />}
-            colorScheme="facebook"
-            bg="#4267B2"
-            _hover={{ bg: "#365899" }}
-            width={{ base: "full", md: "auto" }}
-          >
-            Facebook
-          </Button>
-          <Button
-            leftIcon={<FaGoogle />}
-            colorScheme="red"
-            bg="#DB4437"
-            _hover={{ bg: "#C33D2E" }}
-            width={{ base: "full", md: "auto" }}
-          >
-            Google
-          </Button>
-          <Button
-            leftIcon={<FaLinkedin />}
-            colorScheme="linkedin"
-            bg="#0077B5"
-            _hover={{ bg: "#005B93" }}
-            width={{ base: "full", md: "auto" }}
-          >
-            LinkedIn
-          </Button>
-        </Flex>
-        <Text color="gray.400" mb={4} fontSize={{ base: "sm", md: "md" }}>
-          OR
-        </Text>
         <Input
-          color="white"
+          name="email"
           placeholder="Email"
-          variant="filled"
-          bg="gray.800"
-          border="none"
+          bg="gray.700"
+          color="white"
           mb={4}
-          _hover={{ bg: "gray.700" }}
+          _hover={{ bg: "gray.600" }}
+          onChange={handleInputChange}
         />
-        <Flex position="relative" mb={6} w="100%">
-          <Input
-            color="white"
-            placeholder="Password"
-            variant="filled"
-            bg="gray.800"
-            border="none"
-            type={showPassword ? "text" : "password"}
-            pr="4rem"
-            _hover={{ bg: "gray.700" }}
-          />
-          <Icon
-            as={showPassword ? FaEyeSlash : FaEye}
-            position="absolute"
-            top="50%"
-            right="1rem"
-            transform="translateY(-50%)"
-            color="gray.400"
-            cursor="pointer"
-            _hover={{ color: "white" }}
-            onClick={togglePasswordVisibility}
-          />
-        </Flex>
-        <Button colorScheme="green" w="full" mb={6}>
+        <Input
+          name="password"
+          type="password"
+          placeholder="Password"
+          bg="gray.700"
+          color="white"
+          mb={6}
+          _hover={{ bg: "gray.600" }}
+          onChange={handleInputChange}
+        />
+        <Button colorScheme="green" w="100%" onClick={handleSignIn}>
           Sign In
         </Button>
-        <Text color="gray.400" fontSize={{ base: "xs", md: "sm" }}>
-          Forgot Password?{" "}
-          <Link color="green.400" href="#">
-            Click Here
-          </Link>
-        </Text>
-        <Text color="gray.400" fontSize={{ base: "xs", md: "sm" }} mt={4}>
+        <Text color="gray.400" mt={4} textAlign="center">
           Don&apos;t have an account?{" "}
           <Link color="green.400" href="/signup">
             Sign Up Here
           </Link>
         </Text>
-      </MotionBox>
-
-      {/* Right Side */}
-      <MotionBox
-        flex="1"
-        bgGradient="linear(to-r, green.400, green.600)"
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        px={{ base: 4, md: 8 }}
-        py={{ base: 8, md: 0 }}
-        initial={{ opacity: 0, x: 50 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.8 }}
-      >
-        <Heading color="white" mb={4} fontSize={{ base: "2xl", md: "4xl" }}>
-          New Here?
-        </Heading>
-        <Text
-          color="white"
-          textAlign="center"
-          mb={8}
-          fontSize={{ base: "sm", md: "md" }}
-        >
-          Sign up and discover a great amount of new opportunities!
-        </Text>
-        <Button
-          as={Link}
-          href="/signup"
-          bg="white"
-          color="green.500"
-          _hover={{ bg: "gray.200" }}
-          width={{ base: "full", md: "auto" }}
-        >
-          Sign Up
-        </Button>
       </MotionBox>
     </Flex>
   );
--- pages/signup.js
+++ pages/signup.js
@@ -4,123 +4,189 @@ import {
   Flex,
   Heading,
   Input,
+  Select,
   Text,
-  Link,
+  useToast,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const MotionBox = motion(Box);
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    accountType: "customer", // Default account type
+  });
+  const [additionalData, setAdditionalData] = useState({
+    drivingLicenseId: "",
+    verificationType: "aadhar",
+    verificationId: "",
+  });
+  const toast = useToast();
+
+  // General input change handler
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  // Additional data input change handler for customers
+  const handleAdditionalChange = (e) => {
+    const { name, value } = e.target;
+    setAdditionalData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  // Sign Up button handler
+  const handleSignUp = async () => {
+    const apiEndpoints = {
+      customer: "https://urban-motion-backend.vercel.app/api/customers/add-customer",
+      retailer: "https://urban-motion-backend.vercel.app/api/retailers/add-retailer",
+      admin: "https://urban-motion-backend.vercel.app/api/admins/add-admin",
+    };
+
+    // Prepare the payload based on the account type
+    const payload =
+      formData.accountType === "customer"
+        ? { ...formData, ...additionalData }
+        : formData;
+
+    // Log the payload in JSON format to the terminal
+    console.log("Payload being posted:", JSON.stringify(payload, null, 2));
+
+    try {
+      const response = await fetch(apiEndpoints[formData.accountType], {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      });
+
+      const result = await response.json();
+
+      if (response.ok) {
+        toast({
+          title: "Success",
+          description: result.message || "Account created successfully.",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        throw new Error(result.message || "Error during signup.");
+      }
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
+
   return (
     <Flex
       minH="100vh"
       bg="gray.900"
-      flexDirection={{ base: "column", md: "row" }} // Stack on small screens
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
     >
-      {/* Left Side */}
       <MotionBox
-        flex="1"
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        px={{ base: 4, md: 8 }}
-        py={{ base: 8, md: 0 }} // Add padding on smaller screens
-        initial={{ opacity: 0, x: -50 }}
-        animate={{ opacity: 1, x: 0 }}
+        px={6}
+        py={8}
+        borderRadius="lg"
+        bg="gray.800"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 0.8 }}
+        width="100%"
+        maxWidth="400px"
       >
-        <Heading
-          color="white"
-          mb={4}
-          fontSize={{ base: "2xl", md: "3xl" }} // Responsive font size
-        >
+        <Heading color="white" mb={6} textAlign="center">
           Create an Account
         </Heading>
-        <Text color="gray.400" mb={8} textAlign="center">
-          Sign up to explore more opportunities!
-        </Text>
+        <Select
+          name="accountType"
+          bg="gray.700"
+          color="white"
+          mb={4}
+          _hover={{ bg: "gray.600" }}
+          onChange={handleInputChange}
+        >
+          <option value="customer">Customer</option>
+          <option value="retailer">Retailer</option>
+          <option value="admin">Admin</option>
+        </Select>
         <Input
+          name="name"
           placeholder="Full Name"
-          variant="filled"
-          bg="gray.800"
-          border="none"
+          bg="gray.700"
           color="white"
           mb={4}
-          _hover={{ bg: "gray.700" }}
-          w="100%" // Ensure full width on all screens
+          _hover={{ bg: "gray.600" }}
+          onChange={handleInputChange}
         />
         <Input
+          name="email"
           placeholder="Email"
-          variant="filled"
-          bg="gray.800"
-          border="none"
+          bg="gray.700"
           color="white"
           mb={4}
-          _hover={{ bg: "gray.700" }}
-          w="100%"
+          _hover={{ bg: "gray.600" }}
+          onChange={handleInputChange}
         />
         <Input
+          name="password"
           placeholder="Password"
           type="password"
-          variant="filled"
-          bg="gray.800"
-          border="none"
+          bg="gray.700"
           color="white"
           mb={6}
-          _hover={{ bg: "gray.700" }}
-          w="100%"
+          _hover={{ bg: "gray.600" }}
+          onChange={handleInputChange}
         />
-        <Button colorScheme="green" w="100%" mb={6}>
-          Sign Up
-        </Button>
-        <Text color="gray.400" textAlign="center">
-          Already have an account?{" "}
-          <Link color="green.400" href="/signin">
-            Login Here
-          </Link>
-        </Text>
-      </MotionBox>
 
-      {/* Right Side */}
-      <MotionBox
-        flex="1"
-        bgGradient="linear(to-r, green.400, green.600)"
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        px={{ base: 4, md: 8 }}
-        py={{ base: 8, md: 0 }} // Add padding on smaller screens
-        initial={{ opacity: 0, x: 50 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.8 }}
-      >
-        <Heading
-          color="white"
-          mb={4}
-          fontSize={{ base: "2xl", md: "3xl" }} // Responsive font size
-        >
-          Welcome Back!
-        </Heading>
-        <Text
-          color="white"
-          textAlign="center"
-          mb={8}
-          fontSize={{ base: "sm", md: "md" }} // Adjust text size
-        >
-          Already have an account? Log in to access your opportunities!
-        </Text>
-        <Button
-          bg="white"
-          color="green.500"
-          _hover={{ bg: "gray.200" }}
-          as="a"
-          href="/signin"
-          w="100%" // Full width on smaller screens
-        >
-          Login
+        {/* Conditional fields for Customers */}
+        {formData.accountType === "customer" && (
+          <>
+            <Input
+              name="drivingLicenseId"
+              placeholder="Driving License ID"
+              bg="gray.700"
+              color="white"
+              mb={4}
+              _hover={{ bg: "gray.600" }}
+              onChange={handleAdditionalChange}
+            />
+            <Select
+              name="verificationType"
+              bg="gray.700"
+              color="white"
+              mb={4}
+              _hover={{ bg: "gray.600" }}
+              onChange={handleAdditionalChange}
+            >
+              <option value="aadhar">Aadhar</option>
+              <option value="pan">PAN</option>
+            </Select>
+            <Input
+              name="verificationId"
+              placeholder="Verification ID"
+              bg="gray.700"
+              color="white"
+              mb={4}
+              _hover={{ bg: "gray.600" }}
+              onChange={handleAdditionalChange}
+            />
+          </>
+        )}
+
+        <Button colorScheme="green" w="100%" onClick={handleSignUp}>
+          Sign Up
         </Button>
       </MotionBox>
     </Flex>
